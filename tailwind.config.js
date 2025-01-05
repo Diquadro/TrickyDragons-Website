4a7d11c85db73db86af4f661742905d753427622
Daniele D'Ambrosio <justdosmile@gmail.com>
2025-01-05 18:44:44 +0100

    mobile layout + tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['./dist/**/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            fontSize: {
+                '6xl': ['4rem', '4rem'],
+            },
+        },
     },
     plugins: [],
 }
