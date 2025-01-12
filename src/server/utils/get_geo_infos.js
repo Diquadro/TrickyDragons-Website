64769145b6244681efffac22e74538fd4512be3d
Daniele D'Ambrosio <justdosmile@gmail.com>
2025-01-12 03:05:26 +0100

    country-region-data invalid import 0.2

--- src/server/utils/get_geo_infos.js
+++ src/server/utils/get_geo_infos.js
@@ -1,14 +1,11 @@
 import geoip from 'geoip-lite'
-import { allCountries } from 'country-region-data'
+import countryRegionData from 'country-region-data/dist/data-umd.js'
 
 export default function get_geo_infos(ip_address) {
     const geo_infos = geoip.lookup(ip_address)
-
     if (!geo_infos) return {}
-
-    const country = allCountries?.find((c) => c.countryShortCode === geo_infos.country)
+    const country = countryRegionData?.find((c) => c.countryShortCode === geo_infos.country)
     const region = country?.regions.find((r) => r.shortCode === geo_infos.region)
-
     return {
         country: country?.countryName,
         region: region?.name,
