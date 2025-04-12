31c9047f1af12e357115e541b63a43b1121a2a18
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-04-13 01:23:39 +0200

    Rewrite 2.10

--- src/client/components/umami_analytics/umami_analytics.ts
+++ src/client/components/umami_analytics/umami_analytics.ts
@@ -11,7 +11,7 @@ function loadUmamiAnalytics_prod() {
         const script = document.createElement('script')
         script.defer = true
         script.setAttribute('data-website-id', 'ce981445-5777-42e9-bb76-22050279d2d2')
-        script.src = 'https://analytics.trickydragons.com/script.js'
+        script.src = 'https://cloud.umami.is/script.js'
         document.head.appendChild(script)
     } else {
         console.error('🟥 Umami not loaded: Not on an authorized domain')
@@ -26,7 +26,7 @@ function loadUmamiAnalytics_dev() {
         const script = document.createElement('script')
         script.defer = true
         script.setAttribute('data-website-id', '5819d2e5-b1ca-423b-baf6-101627f0ff4d')
-        script.src = 'https://analytics.trickydragons.com/script.js'
+        script.src = 'https://cloud.umami.is/script.js'
         document.head.appendChild(script)
     } else {
         console.error('🟥 Umami not loaded: Not on an authorized domain')
