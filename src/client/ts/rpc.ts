4c0bc04e259c795c81a4a483dc7817881f8b41f9
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-04-23 18:20:05 +0200

    Fix - api callout

--- src/client/ts/rpc.ts
+++ src/client/ts/rpc.ts
@@ -3,13 +3,17 @@ import {
     Subscribe_Contacts_Response,
     subscribe_contacts_response_schema,
 } from '@shared/validations/subscribe_contact.validations'
-import { API } from '@shared/constants/app.constants'
+import { API, ENV } from '@shared/constants/app.constants'
 
 export class RPC {
     static async subscribe_contacts(
         contacts: Subscribe_Contacts_Request,
     ): Promise<Subscribe_Contacts_Response> {
-        const response = await fetch(`${API.ENDPOINTS.CONTACTS.SUBSCRIBE}`, {
+        const endpoint = ENV.LOCAL
+            ? `${API.ENDPOINTS.CONTACTS.SUBSCRIBE}`
+            : `${API.URL}${API.ENDPOINTS.CONTACTS.SUBSCRIBE}`
+
+        const response = await fetch(endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -21,7 +25,11 @@ export class RPC {
             throw new Error(`Request failed with status ${response.status} - ${await response.text()}`)
         }
 
-        const json = await response.json()
-        return subscribe_contacts_response_schema.parse(json)
+        try {
+            const json = await response.json()
+            return subscribe_contacts_response_schema.parse(json)
+        } catch (error) {
+            throw new Error(`Invalid JSON response: ${await response.text()}`)
+        }
     }
 }
