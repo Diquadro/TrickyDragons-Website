b61fa2b3b81349e4b838ac5c53efb9dd8e8b8ba9
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-04-27 20:22:34 +0200

    added posthog

---
+++ src/client/components/posthog/posthog.ts
@@ -0,0 +1,25 @@
+import { ENV } from '@shared/constants/app.constants'
+import posthog from 'posthog-js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    load_posthog_dev()
+    load_posthog_prod()
+})
+
+function load_posthog_prod() {
+    if (ENV.PRODUCTION) {
+    } else {
+        console.error('🟥 Posthog not loaded: Not on an authorized domain')
+    }
+}
+
+function load_posthog_dev() {
+    if (ENV.DEVELOPMENT) {
+        posthog.init('phc_PSxWVH6wEiaYVhtH6uTru9gKTbzbKXMXiHPRFJ6CGsC', {
+            api_host: 'https://eu.i.posthog.com',
+            person_profiles: 'always',
+        })
+    } else {
+        console.error('🟥 Posthog not loaded: Not on an authorized domain')
+    }
+}
--- src/client/layouts/layout_common/layout_common.ts
+++ src/client/layouts/layout_common/layout_common.ts
@@ -1,2 +1,3 @@
 import '@client/components/umami/umami'
+import '@client/components/posthog/posthog'
 import '@client/components/cookie_consent/cookieconsent-config'
