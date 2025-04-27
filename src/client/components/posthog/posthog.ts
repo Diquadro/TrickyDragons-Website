7a04f5173c5c9265d2d70be1b7ae0294a2793a5f
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-04-27 20:56:47 +0200

    posthog for production

--- src/client/components/posthog/posthog.ts
+++ src/client/components/posthog/posthog.ts
@@ -2,23 +2,19 @@ import { ENV } from '@shared/constants/app.constants'
 import posthog from 'posthog-js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    load_posthog_dev()
     load_posthog_prod()
 })
 
 function load_posthog_prod() {
     if (ENV.PRODUCTION) {
-    } else {
-        console.error('🟥 Posthog not loaded: Not on an authorized domain')
-    }
-}
-
-function load_posthog_dev() {
-    if (ENV.DEVELOPMENT) {
         posthog.init('phc_PSxWVH6wEiaYVhtH6uTru9gKTbzbKXMXiHPRFJ6CGsC', {
             api_host: 'https://eu.i.posthog.com',
             person_profiles: 'always',
         })
+
+        if (localStorage.getItem('umami.disabled') === 'true') {
+            posthog.opt_out_capturing()
+        }
     } else {
         console.error('🟥 Posthog not loaded: Not on an authorized domain')
     }
