204127a945a1e32c2f1c76000d1594a4e76db235
Daniele D'Ambrosio <justdosmile@gmail.com>
2025-06-04 23:46:16 +0200

    name trigger

--- src/database/migrations/00025_trigger_on_analytics_events.sql
+++ src/database/migrations/00025_trigger_on_analytics_events.sql
@@ -1,1 +1,1 @@
-CREATE OR REPLACE TRIGGER audit_analytics_events_trg BEFORE INSERT OR UPDATE ON public.analytics_events FOR EACH ROW EXECUTE FUNCTION create_audit_trigger_handler();
+CREATE OR REPLACE TRIGGER audit_analytics_events_trg BEFORE INSERT OR UPDATE ON public.analytics_events FOR EACH ROW EXECUTE FUNCTION audit_fields_handler();
