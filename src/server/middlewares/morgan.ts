1537f5f83536bec8cfc300bd8dc2bfcc3e7ef0cd
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-08-12 01:05:30 +0200

    remove extended debug logging from morgan middleware to streamline request handling

--- src/server/middlewares/morgan.ts
+++ src/server/middlewares/morgan.ts
@@ -4,26 +4,6 @@ import morgan from 'morgan'
 // Custom token to extract referrer page without query params
 morgan.token('referrer-page', (req: Request) => {
     const referrer = req.get('Referer') || req.get('Referrer')
-
-    // EXTENDED DEBUG: Log more info
-    console.log('🔍 EXTENDED DEBUG:', {
-        'Request URL': req.url,
-        'Request Method': req.method,
-        'Referer header': req.get('Referer'),
-        'Referrer header': req.get('Referrer'),
-        'Origin header': req.get('Origin'),
-        'Host header': req.get('Host'),
-        'X-Forwarded-Host': req.get('X-Forwarded-Host'),
-        'X-Forwarded-Proto': req.get('X-Forwarded-Proto'),
-        'Referrer-Policy': req.get('Referrer-Policy'),
-        'All Headers': Object.keys(req.headers)
-            .filter((h) => h.toLowerCase().includes('ref'))
-            .reduce((acc, key) => {
-                acc[key] = req.headers[key]
-                return acc
-            }, {} as any),
-    })
-
     if (!referrer) return '-'
 
     try {
