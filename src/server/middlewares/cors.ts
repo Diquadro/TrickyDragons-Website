5a90343084fc4a52346b70a1728fff68d54e0f5d
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-08-11 23:33:22 +0200

    fix 0.3

--- src/server/middlewares/cors.ts
+++ src/server/middlewares/cors.ts
@@ -4,7 +4,7 @@ import { ENV, CLIENT, API } from '@shared/constants/app.constants'
 // CORS middleware configuration
 // Enables Cross-Origin Resource Sharing with appropriate settings
 export const cors_middleware = cors({
-    origin: ENV.PRODUCTION || ENV.DEVELOPMENT ? [CLIENT.URL, API.URL] : '*',
+    origin: ENV.LOCAL ? '*' : [CLIENT.URL, API.URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
