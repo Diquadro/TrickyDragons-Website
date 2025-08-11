fa1b6f71b9076e4fbd08316f6c6671811a126271
Daniele D'Ambrosio <daniele.diquadro@gmail.com>
2025-08-11 23:40:18 +0200

    fix umami

--- src/client/ts/umami.ts
+++ src/client/ts/umami.ts
@@ -1,32 +1,17 @@
 import { ENV } from '@shared/constants/app.constants'
 
 export function initialize_umami() {
-    document.addEventListener('DOMContentLoaded', () => {
-        load_umami_dev()
-        load_umami_prod()
-    })
+    load_umami()
 }
 
-function load_umami_prod() {
+function load_umami() {
+    const script = document.createElement('script')
+    script.defer = true
     if (ENV.PRODUCTION) {
-        const script = document.createElement('script')
-        script.defer = true
         script.setAttribute('data-website-id', 'ce981445-5777-42e9-bb76-22050279d2d2')
-        script.src = 'https://cloud.umami.is/script.js'
-        document.head.appendChild(script)
     } else {
-        console.error('🟥 Umami not loaded: Not on an authorized domain')
-    }
-}
-
-function load_umami_dev() {
-    if (ENV.DEVELOPMENT || ENV.LOCAL) {
-        const script = document.createElement('script')
-        script.defer = true
         script.setAttribute('data-website-id', '5819d2e5-b1ca-423b-baf6-101627f0ff4d')
-        script.src = 'https://cloud.umami.is/script.js'
-        document.head.appendChild(script)
-    } else {
-        console.error('🟥 Umami not loaded: Not on an authorized domain')
     }
+    script.src = 'https://cloud.umami.is/script.js'
+    document.head.appendChild(script)
 }
